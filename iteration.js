9a471f828058d158375ee5205218c473dbec2321
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2021-11-02 12:10:25 +0000

    First debug

--- iteration.js
+++ iteration.js
@@ -1,9 +1,8 @@
 a=5;
--    = ["Tunis","Tripoli","Alger","Rabbat","NoakShot","Cairo"]; 
+cities = ["Tunis","Tripoli","Alger","Rabbat","NoakShot","Cairo"]
 
 for(var i=0;i<cities.length;i++){
     console.log(cities[i]);
-   // i=i+1;
 }
 
 
